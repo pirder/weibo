b27ec97befbc32f25f08c9a433fc481feec4cb57
pirders <pirders@pirdersdeiMac.local>
2017-08-11 17:04:11 +0800

    Initial Commit

---
+++ iOS微博Tests/iOS__Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS__Tests.swift
+//  iOS微博Tests
+//
+//  Created by pirders on 2017/8/11.
+//  Copyright © 2017年 pirders. All rights reserved.
+//
+
+import XCTest
+@testable import iOS__
+
+class iOS__Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
