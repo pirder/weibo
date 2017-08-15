84911824255946ac96c5882d620c7250877e1626
pirders <pirders@pirdersdeiMac.local>
2017-08-15 22:16:57 +0800

    增加了便利构造函数（）2个 ，添加4个小视图

---
+++ iOS微博/Classes/Tools/Extensions/UIimage+UIButton+Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  UIimage+Extensions.swift
+//  iOS微博
+//
+//  Created by pirders on 2017/8/15.
+//  Copyright © 2017年 pirders. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel{
+
+    convenience init(title: String,fontsize: Int,color: UIColor ){
+    self.init()
+        text = title
+        font = UIFont.systemFont(ofSize: CGFloat(fontsize))
+        textColor = color
+        sizeToFit()
+//    self.init()
+        
+    }
+}
+extension UIButton{
+    
+    convenience init(title: String,fontsize: Int,normalColor: UIColor,highlighColor: UIColor,image: String ) {
+        self.init()
+                setTitle(title, for: .normal)
+                setTitleColor(normalColor, for: .normal)
+                setTitleColor(highlighColor , for: .highlighted)
+                 titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(fontsize))
+                 sizeToFit()
+        setBackgroundImage( UIImage( named: image), for: .normal)
+    }
+       
+
+
+}
--- iOS微博/Classes/View/Main/VisitorView/WBVisitorVIew.swift
+++ iOS微博/Classes/View/Main/VisitorView/WBVisitorVIew.swift
@@ -20,6 +20,23 @@ class WBVisitorVIew: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+  
+    //图像视图
+    fileprivate lazy var iconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    //背景动画
+  fileprivate lazy var houseIconView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+    //标签
+   fileprivate lazy var tipLabel = UILabel(title: "sajdasjclxjklkc", fontsize: 15, color: UIColor.darkGray)
+    
+    
+    //注册按钮
+    fileprivate lazy var registerButton: UIButton = UIButton(title: "注册", fontsize: 18, normalColor: UIColor.orange, highlighColor: UIColor.darkGray,image: "common_button_white_disable")
+   
+
+    
+    //登录按钮
+   fileprivate lazy var loginButton: UIButton = UIButton(title: "登录", fontsize: 18, normalColor: UIColor.darkGray, highlighColor: UIColor.black,image: "common_button_white_disable")
+    
 }
 
 
@@ -29,6 +46,11 @@ extension WBVisitorVIew {
     func setupUI(){
     
     backgroundColor = UIColor.white
+        addSubview(iconView)
+        addSubview(houseIconView)
+        addSubview(tipLabel)
+        addSubview(registerButton)
+        addSubview(loginButton)
     }
     
 }
