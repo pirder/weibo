49a861169cfb18e74d526a5dc9014cc95c18b67b
pirders <pirders@pirdersdeiMac.local>
2017-08-18 00:16:35 +0800

    网络管理工具单例

---
+++ iOS微博/Classes/Tools/Network/WBNetworkMenage.swift
@@ -0,0 +1,18 @@
+//
+//  WBNetworkMenage.swift
+//  iOS微博
+//
+//  Created by pirders on 2017/8/18.
+//  Copyright © 2017年 pirders. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+//网络管理
+class WBNetworkMenage: AFHTTPSessionManager {
+
+   ///静态区域 ／常量／闭包
+    static let shared = WBNetworkMenage()
+    
+}
